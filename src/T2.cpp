d4a07ee1c3ac75e62b59bdbf8f9dd37fc54da0ad
Francisca <faconcha@dcc.uchile.cl>
2015-04-16 17:12:57 -0300

    Ya está tomando aproximadamente 3 frames por segundo. El aproximadamente es por culpa de OpenCV que tiene imprecisiones al momento de entregar los valores de fps y total de frames de un video. En general entrega ~10 frames más de los que debería.

--- src/T2.cpp
+++ src/T2.cpp
@@ -22,67 +22,72 @@ using namespace cv;
 vector<Descriptor> getVideoDescriptor(VideoCapture capture){
 	vector<Descriptor> videoDescriptor;
 
-	int fps = capture.get(CV_CAP_PROP_FPS);
-	int totalFrames = capture.get(CV_CAP_PROP_FRAME_COUNT);
+	double fps = capture.get(CV_CAP_PROP_FPS);
+	double totalFrames = capture.get(CV_CAP_PROP_FRAME_COUNT);
+	/*double length = totalFrames / fps;
 
-	cout << "fps " << fps << endl;
-	cout << "fps/3 = " << fps/3 << endl;
-	cout << "total " << totalFrames << endl;
+	cout << "Video dura " << length << " segundos." << endl;
+	cout << "Deberia tener " << length*3 << " frames." << endl;
 
-	cout << "width: " << capture.get(CV_CAP_PROP_FRAME_WIDTH) << endl;
-	cout << "height: " << capture.get(CV_CAP_PROP_FRAME_HEIGHT) << endl;
+	cout << "fps " << fps << endl;*/
+
+	int skipFrames = fps/3 - 1;
+	int cntFrames = 0;
 
 	Mat frame, grayFrame;
-	for (int j = 0; j <= totalFrames; j++) {
-		if (!capture.grab() || !capture.retrieve(frame))
-			break;
-		cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
-		int frameWidth = capture.get(CV_CAP_PROP_FRAME_WIDTH);
-		int frameHeight = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
+	for (int j = 1; j <= totalFrames; j++) {
+		if (j % skipFrames == 0) {
+			if (!capture.grab() || !capture.retrieve(frame))
+				break;
 
-		int areaWidth = frameWidth/2;
-		int areaHeight = frameHeight/2;
-		Mat area1, area2, area3, area4;
-		/* | 1 | 2 |
-		 * | 3 | 4 |
-		 */
-		area1 = grayFrame(Rect(0, 0, areaWidth, areaHeight));
-		area2 = grayFrame(Rect(areaWidth, 0, areaWidth, areaHeight));
-		area3 = grayFrame(Rect(0, areaHeight, areaWidth, areaHeight));
-		area4 = grayFrame(Rect(areaWidth, areaHeight, areaWidth, areaHeight));
+			cvtColor(frame, grayFrame, COLOR_BGR2GRAY);
+			int frameWidth = capture.get(CV_CAP_PROP_FRAME_WIDTH);
+			int frameHeight = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
 
-		//Histogram
-		int histSize = 128;
-		float range[] = {0, 256}; //the upper boundary is exclusive
-		const float* histRange = {range};
-		bool uniform = true; bool accumulate = false;
-		Mat hist1, hist2, hist3, hist4;
-		// Calculate histogram for each area
-		calcHist(&area1, 1, 0, Mat(), hist1, 1, &histSize, &histRange, uniform, accumulate);
-		calcHist(&area2, 1, 0, Mat(), hist2, 1, &histSize, &histRange, uniform, accumulate);
-		calcHist(&area3, 1, 0, Mat(), hist3, 1, &histSize, &histRange, uniform, accumulate);
-		calcHist(&area4, 1, 0, Mat(), hist4, 1, &histSize, &histRange, uniform, accumulate);
+			int areaWidth = frameWidth / 2;
+			int areaHeight = frameHeight / 2;
+			Mat area1, area2, area3, area4;
+			/* | 1 | 2 |
+		 	 * | 3 | 4 |
+		 	 */
+			area1 = grayFrame(Rect(0, 0, areaWidth, areaHeight));
+			area2 = grayFrame(Rect(areaWidth, 0, areaWidth, areaHeight));
+			area3 = grayFrame(Rect(0, areaHeight, areaWidth, areaHeight));
+			area4 = grayFrame(Rect(areaWidth, areaHeight, areaWidth, areaHeight));
 
-		// Create images to show histograms
-		int hist_h = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
-		int bin_w = cvRound((double)frameWidth/histSize);
-		Mat histImage1(hist_h, frameWidth, CV_8UC3, Scalar(0, 0, 0));
-		Mat histImage2(hist_h, frameWidth, CV_8UC3, Scalar(0, 0, 0));
-		Mat histImage3(hist_h, frameWidth, CV_8UC3, Scalar(0, 0, 0));
-		Mat histImage4(hist_h, frameWidth, CV_8UC3, Scalar(0, 0, 0));
+			//Histogram
+			int histSize = 128;
+			float range[] = {0, 256}; //the upper boundary is exclusive
+			const float *histRange = {range};
+			bool uniform = true;
+			bool accumulate = false;
+			Mat hist1, hist2, hist3, hist4;
+			// Calculate histogram for each area
+			calcHist(&area1, 1, 0, Mat(), hist1, 1, &histSize, &histRange, uniform, accumulate);
+			calcHist(&area2, 1, 0, Mat(), hist2, 1, &histSize, &histRange, uniform, accumulate);
+			calcHist(&area3, 1, 0, Mat(), hist3, 1, &histSize, &histRange, uniform, accumulate);
+			calcHist(&area4, 1, 0, Mat(), hist4, 1, &histSize, &histRange, uniform, accumulate);
 
-		// Normalize each histrogram
-		normalize(hist1, hist1, 0, histImage1.rows, NORM_MINMAX, -1, Mat());
-		normalize(hist2, hist2, 0, histImage2.rows, NORM_MINMAX, -1, Mat());
-		normalize(hist3, hist3, 0, histImage3.rows, NORM_MINMAX, -1, Mat());
-		normalize(hist4, hist4, 0, histImage4.rows, NORM_MINMAX, -1, Mat());
+			// Create images to show histograms
+			int hist_h = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
+			Mat histImage1(hist_h, frameWidth, CV_8UC3, Scalar(0, 0, 0));
+			Mat histImage2(hist_h, frameWidth, CV_8UC3, Scalar(0, 0, 0));
+			Mat histImage3(hist_h, frameWidth, CV_8UC3, Scalar(0, 0, 0));
+			Mat histImage4(hist_h, frameWidth, CV_8UC3, Scalar(0, 0, 0));
 
-		// Frame descriptor: the 4 histograms
-		Descriptor frameDescriptor(hist1, hist2, hist3, hist4);
-		videoDescriptor.push_back(frameDescriptor);
+			// Normalize each histrogram
+			normalize(hist1, hist1, 0, histImage1.rows, NORM_MINMAX, -1, Mat());
+			normalize(hist2, hist2, 0, histImage2.rows, NORM_MINMAX, -1, Mat());
+			normalize(hist3, hist3, 0, histImage3.rows, NORM_MINMAX, -1, Mat());
+			normalize(hist4, hist4, 0, histImage4.rows, NORM_MINMAX, -1, Mat());
 
-		// Draw histograms
-		/*for(int i = 1; i < histSize; i++ ){
+			// Frame descriptor: the 4 histograms
+			Descriptor frameDescriptor(hist1, hist2, hist3, hist4);
+			videoDescriptor.push_back(frameDescriptor);
+
+			// Draw histograms
+			/*int bin_w = cvRound((double) frameWidth / histSize);
+			for(int i = 1; i < histSize; i++ ){
 			line(histImage1, Point( bin_w*(i-1), hist_h - cvRound(hist1.at<float>(i-1)) ) ,
 							 Point( bin_w*(i), hist_h - cvRound(hist1.at<float>(i)) ),
 							 Scalar( 255, 0, 0), 2, 8, 0  );
@@ -103,13 +108,13 @@ vector<Descriptor> getVideoDescriptor(VideoCapture capture){
 							 Scalar( 255, 0, 0), 2, 8, 0  );
 		}*/
 
-		// Create big image to display all histograms
-		/*Mat dst(frameHeight, frameWidth, CV_8UC3);
+			// Create big image to display all histograms
+			/*Mat dst(frameHeight, frameWidth, CV_8UC3);
 		Mat h1 = Mat(dst, Range(areaHeight, frameHeight), Range(areaWidth, frameWidth));
 		histImage1.copyTo(h1);*/
 
 
-		/*imshow("histogram1", histImage1);
+			/*imshow("histogram1", histImage1);
 		imshow("histogram2", histImage2);
 		imshow("histogram3", histImage3);
 		imshow("histogram4", histImage4);
@@ -118,7 +123,8 @@ vector<Descriptor> getVideoDescriptor(VideoCapture capture){
 		char c = waitKey(33);
 		if (c == 27)
 			break;*/
-
+			cntFrames++;
+		}
 	}
 	return videoDescriptor;
 }
@@ -152,7 +158,6 @@ int main(int _nargs, char** _vargs)
 	}
 
 	videoDescriptor = getVideoDescriptor(capture);
-
 	
 	// Index calculation (for the searched file)
 	Index index = Index();
@@ -166,8 +171,6 @@ int main(int _nargs, char** _vargs)
 		
 		VideoCapture capture;
 		capture.open(location);
-
-		int fps = capture.get(CV_CAP_PROP_FPS);
 
 		/**
 		 * Magic
